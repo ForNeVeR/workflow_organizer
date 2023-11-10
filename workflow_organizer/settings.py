ecfa1a98a84a682cddbf94d70d36a5f7e4726833
“Ruslan <callogan217@gmail.com>
2023-11-10 14:47:27 +0200

    Creation of directory with all static files and connecting them to the project

--- workflow_organizer/settings.py
+++ workflow_organizer/settings.py
@@ -132,6 +132,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = "staticfiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -140,3 +146,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 TAGGIT_CASE_INSENSITIVE = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DJANGO_BOOTSTRAP_CUSTOM_CSS_OVERRIDES = {"--bs-progress-height": None}
