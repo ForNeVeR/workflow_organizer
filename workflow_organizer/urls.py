987762a6146646ace521a4f7ab86092a69d7e722
“Ruslan <callogan217@gmail.com>
2023-11-10 14:32:21 +0200

    Creation of local urls

--- workflow_organizer/urls.py
+++ workflow_organizer/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("", include("assignment_handler.urls", namespace="assignment_handler")),
 ]
